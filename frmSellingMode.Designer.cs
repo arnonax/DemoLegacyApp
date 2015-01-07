3402cea2022bd9dc65752543c79c1e529cc108a7
arnonax <arnonaxelrod@hotmail.com>
2015-01-07 16:22:49 +0200

    Selling mode working!

--- frmSellingMode.Designer.cs
+++ frmSellingMode.Designer.cs
@@ -28,14 +28,19 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
 			this.label1 = new System.Windows.Forms.Label();
 			this.txtBarcode = new System.Windows.Forms.TextBox();
 			this.btnAdd = new System.Windows.Forms.Button();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.dgvInvoice = new System.Windows.Forms.DataGridView();
 			this.label2 = new System.Windows.Forms.Label();
 			this.txtTotal = new System.Windows.Forms.TextBox();
 			this.btnClearAll = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.productsTableAdapter = new LegacyApplication.StoreDataDataSetTableAdapters.ProductsTableAdapter();
+			this.colBarcode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.colDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.colPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.dgvInvoice)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -53,7 +58,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtBarcode.Location = new System.Drawing.Point(83, 21);
 			this.txtBarcode.Name = "txtBarcode";
-			this.txtBarcode.Size = new System.Drawing.Size(291, 22);
+			this.txtBarcode.Size = new System.Drawing.Size(448, 22);
 			this.txtBarcode.TabIndex = 1;
 			this.txtBarcode.TextChanged += new System.EventHandler(this.txtBarcode_TextChanged);
 			// 
@@ -61,7 +66,7 @@
 			// 
 			this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnAdd.Enabled = false;
-			this.btnAdd.Location = new System.Drawing.Point(380, 13);
+			this.btnAdd.Location = new System.Drawing.Point(537, 13);
 			this.btnAdd.Name = "btnAdd";
 			this.btnAdd.Size = new System.Drawing.Size(113, 38);
 			this.btnAdd.TabIndex = 2;
@@ -69,26 +74,30 @@
 			this.btnAdd.UseVisualStyleBackColor = true;
 			this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
 			// 
-			// dataGridView1
+			// dgvInvoice
 			// 
-			this.dataGridView1.AllowUserToAddRows = false;
-			this.dataGridView1.AllowUserToDeleteRows = false;
-			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.dgvInvoice.AllowUserToAddRows = false;
+			this.dgvInvoice.AllowUserToDeleteRows = false;
+			this.dgvInvoice.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(12, 58);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.ReadOnly = true;
-			this.dataGridView1.RowTemplate.Height = 24;
-			this.dataGridView1.Size = new System.Drawing.Size(481, 341);
-			this.dataGridView1.TabIndex = 3;
+			this.dgvInvoice.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvInvoice.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colBarcode,
+            this.colDescription,
+            this.colPrice});
+			this.dgvInvoice.Location = new System.Drawing.Point(12, 58);
+			this.dgvInvoice.Name = "dgvInvoice";
+			this.dgvInvoice.ReadOnly = true;
+			this.dgvInvoice.RowTemplate.Height = 24;
+			this.dgvInvoice.Size = new System.Drawing.Size(638, 341);
+			this.dgvInvoice.TabIndex = 3;
 			// 
 			// label2
 			// 
 			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(282, 422);
+			this.label2.Location = new System.Drawing.Point(439, 422);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(44, 17);
 			this.label2.TabIndex = 4;
@@ -98,7 +107,7 @@
 			// 
 			this.txtTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtTotal.Location = new System.Drawing.Point(332, 416);
+			this.txtTotal.Location = new System.Drawing.Point(489, 416);
 			this.txtTotal.Name = "txtTotal";
 			this.txtTotal.ReadOnly = true;
 			this.txtTotal.Size = new System.Drawing.Size(161, 26);
@@ -113,23 +122,51 @@
 			this.btnClearAll.TabIndex = 6;
 			this.btnClearAll.Text = "Clear all";
 			this.btnClearAll.UseVisualStyleBackColor = true;
+			this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+			// 
+			// productsTableAdapter
+			// 
+			this.productsTableAdapter.ClearBeforeFill = true;
+			// 
+			// colBarcode
+			// 
+			this.colBarcode.HeaderText = "Barcode";
+			this.colBarcode.Name = "colBarcode";
+			this.colBarcode.ReadOnly = true;
+			// 
+			// colDescription
+			// 
+			this.colDescription.HeaderText = "Description";
+			this.colDescription.Name = "colDescription";
+			this.colDescription.ReadOnly = true;
+			this.colDescription.Width = 200;
+			// 
+			// colPrice
+			// 
+			dataGridViewCellStyle1.Format = "C2";
+			dataGridViewCellStyle1.NullValue = null;
+			this.colPrice.DefaultCellStyle = dataGridViewCellStyle1;
+			this.colPrice.HeaderText = "Price";
+			this.colPrice.Name = "colPrice";
+			this.colPrice.ReadOnly = true;
 			// 
 			// frmSellingMode
 			// 
 			this.AcceptButton = this.btnAdd;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(505, 453);
+			this.ClientSize = new System.Drawing.Size(662, 453);
 			this.Controls.Add(this.btnClearAll);
 			this.Controls.Add(this.txtTotal);
 			this.Controls.Add(this.label2);
-			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.dgvInvoice);
 			this.Controls.Add(this.btnAdd);
 			this.Controls.Add(this.txtBarcode);
 			this.Controls.Add(this.label1);
 			this.Name = "frmSellingMode";
 			this.Text = "Selling mode";
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.Load += new System.EventHandler(this.frmSellingMode_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dgvInvoice)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -140,9 +177,13 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.TextBox txtBarcode;
 		private System.Windows.Forms.Button btnAdd;
-		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridView dgvInvoice;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.TextBox txtTotal;
 		private System.Windows.Forms.Button btnClearAll;
+		private StoreDataDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+		private System.Windows.Forms.DataGridViewTextBoxColumn colBarcode;
+		private System.Windows.Forms.DataGridViewTextBoxColumn colDescription;
+		private System.Windows.Forms.DataGridViewTextBoxColumn colPrice;
 	}
 }
--- frmSellingMode.cs
+++ frmSellingMode.cs
@@ -6,6 +6,9 @@ namespace LegacyApplication
 {
 	public partial class frmSellingMode : Form
 	{
+		private readonly StoreDataSet _dataset = new StoreDataSet();
+		private decimal _total;
+
 		public frmSellingMode()
 		{
 			InitializeComponent();
@@ -18,13 +21,32 @@ namespace LegacyApplication
 
 		private void btnAdd_Click(object sender, EventArgs e)
 		{
-			var dataset = new StoreDataSet();
-			var product = dataset.Products.FirstOrDefault(p => p.Barcode == txtBarcode.Text);
+			var product = _dataset.Products.FirstOrDefault(p => p.Barcode == txtBarcode.Text);
 			if (product == null)
+			{
 				MessageBox.Show("No product found for the specified barcode", "Selling", MessageBoxButtons.OK,
 					MessageBoxIcon.Asterisk);
+				return;
+			}
 
+			dgvInvoice.Rows.Add(product.Barcode, product.Description, product.Price);
+			txtBarcode.SelectAll();
 
+			_total += product.Price;
+			txtTotal.Text = _total.ToString("C");
+		}
+
+		private void frmSellingMode_Load(object sender, EventArgs e)
+		{
+			productsTableAdapter.Fill(_dataset.Products);
+			txtTotal.Text = 0m.ToString("C");
+		}
+
+		private void btnClearAll_Click(object sender, EventArgs e)
+		{
+			dgvInvoice.Rows.Clear();
+			_total = 0;
+			txtTotal.Text = 0m.ToString("C");
 		}
 	}
 }
