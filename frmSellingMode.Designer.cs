d5b93b681bb83e292b34ba4d677e3dc01b9668c1
arnonax <arnonaxelrod@hotmail.com>
2015-01-08 17:56:11 +0200

    (Simple) Promotions work

--- frmSellingMode.Designer.cs
+++ frmSellingMode.Designer.cs
@@ -40,7 +40,13 @@
 			this.txtTotal = new System.Windows.Forms.TextBox();
 			this.btnClearAll = new System.Windows.Forms.Button();
 			this.productsTableAdapter = new LegacyApplication.StoreDataSetTableAdapters.ProductsTableAdapter();
+			this.dgvPromotions = new System.Windows.Forms.DataGridView();
+			this.colPromotionDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.colDiscount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.label3 = new System.Windows.Forms.Label();
+			this.promotionsTableAdapter = new LegacyApplication.StoreDataSetTableAdapters.PromotionsTableAdapter();
 			((System.ComponentModel.ISupportInitialize)(this.dgvInvoice)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dgvPromotions)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -90,7 +96,7 @@
 			this.dgvInvoice.Name = "dgvInvoice";
 			this.dgvInvoice.ReadOnly = true;
 			this.dgvInvoice.RowTemplate.Height = 24;
-			this.dgvInvoice.Size = new System.Drawing.Size(638, 341);
+			this.dgvInvoice.Size = new System.Drawing.Size(638, 217);
 			this.dgvInvoice.TabIndex = 3;
 			// 
 			// colBarcode
@@ -150,12 +156,54 @@
 			// 
 			this.productsTableAdapter.ClearBeforeFill = true;
 			// 
+			// dgvPromotions
+			// 
+			this.dgvPromotions.AllowUserToAddRows = false;
+			this.dgvPromotions.AllowUserToDeleteRows = false;
+			this.dgvPromotions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvPromotions.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colPromotionDescription,
+            this.colDiscount});
+			this.dgvPromotions.Location = new System.Drawing.Point(12, 308);
+			this.dgvPromotions.Name = "dgvPromotions";
+			this.dgvPromotions.ReadOnly = true;
+			this.dgvPromotions.RowTemplate.Height = 24;
+			this.dgvPromotions.Size = new System.Drawing.Size(638, 91);
+			this.dgvPromotions.TabIndex = 7;
+			// 
+			// colPromotionDescription
+			// 
+			this.colPromotionDescription.HeaderText = "Description";
+			this.colPromotionDescription.Name = "colPromotionDescription";
+			this.colPromotionDescription.ReadOnly = true;
+			// 
+			// colDiscount
+			// 
+			this.colDiscount.HeaderText = "Discount";
+			this.colDiscount.Name = "colDiscount";
+			this.colDiscount.ReadOnly = true;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 282);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(74, 17);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "Discounts:";
+			// 
+			// promotionsTableAdapter
+			// 
+			this.promotionsTableAdapter.ClearBeforeFill = true;
+			// 
 			// frmSellingMode
 			// 
 			this.AcceptButton = this.btnAdd;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(662, 453);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.dgvPromotions);
 			this.Controls.Add(this.btnClearAll);
 			this.Controls.Add(this.txtTotal);
 			this.Controls.Add(this.label2);
@@ -167,6 +215,7 @@
 			this.Text = "Selling mode";
 			this.Load += new System.EventHandler(this.frmSellingMode_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dgvInvoice)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dgvPromotions)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -185,5 +234,10 @@
 		private System.Windows.Forms.DataGridViewTextBoxColumn colBarcode;
 		private System.Windows.Forms.DataGridViewTextBoxColumn colDescription;
 		private System.Windows.Forms.DataGridViewTextBoxColumn colPrice;
+		private System.Windows.Forms.DataGridView dgvPromotions;
+		private System.Windows.Forms.DataGridViewTextBoxColumn colPromotionDescription;
+		private System.Windows.Forms.DataGridViewTextBoxColumn colDiscount;
+		private System.Windows.Forms.Label label3;
+		private StoreDataSetTableAdapters.PromotionsTableAdapter promotionsTableAdapter;
 	}
 }
--- frmSellingMode.cs
+++ frmSellingMode.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -7,7 +8,7 @@ namespace LegacyApplication
 	public partial class frmSellingMode : Form
 	{
 		private readonly StoreDataSet _dataset = new StoreDataSet();
-		private decimal _total;
+		private readonly List<StoreDataSet.ProductsRow> _productsInInvoice = new List<StoreDataSet.ProductsRow>();
 
 		public frmSellingMode()
 		{
@@ -29,23 +30,44 @@ namespace LegacyApplication
 				return;
 			}
 
+			_productsInInvoice.Add(product);
 			dgvInvoice.Rows.Add(product.Barcode, product.Description, product.Price);
 			txtBarcode.SelectAll();
 
-			_total += product.Price;
-			txtTotal.Text = _total.ToString("C");
+			var total = _productsInInvoice.Sum(x => x.Price);
+			total -= CalculateDiscounts();
+
+			txtTotal.Text = total.ToString("C");
+		}
+
+		private decimal CalculateDiscounts()
+		{
+			var totalDisount = 0m;
+			dgvPromotions.Rows.Clear();
+			foreach (var promotion in _dataset.Promotions)
+			{
+				var actualQuantity = _productsInInvoice.Count(x => x.Id == promotion.ProductId);
+				if (actualQuantity >= promotion.Quantity)
+				{
+					dgvPromotions.Rows.Add(promotion.Description, promotion.Discount);
+					totalDisount += promotion.Discount;
+				}
+			}
+			return totalDisount;
 		}
 
 		private void frmSellingMode_Load(object sender, EventArgs e)
 		{
 			productsTableAdapter.Fill(_dataset.Products);
+			promotionsTableAdapter.Fill(_dataset.Promotions);
 			txtTotal.Text = 0m.ToString("C");
 		}
 
 		private void btnClearAll_Click(object sender, EventArgs e)
 		{
 			dgvInvoice.Rows.Clear();
-			_total = 0;
+			dgvPromotions.Rows.Clear();
+			_productsInInvoice.Clear();
 			txtTotal.Text = 0m.ToString("C");
 		}
 	}
